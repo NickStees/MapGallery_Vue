2b5944e04e10677c0f23486b62a52d6e055d5ecb
Nick Stees <nstees@gmail.com>
2018-05-29 22:15:51 -0400

    Improved filter to evaluate descriptions

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -78,9 +78,14 @@ export default {
     filterMaps(map) {
       const strTags = map.tags.join(" ").toLowerCase();
       const lowercaseName = map.title.toLowerCase();
-      const selection = strTags + lowercaseName;
+      const description = map.description ? map.description.toLowerCase() : '';
+      const selection = strTags + lowercaseName + description;
       const lowercaseQuery = this.filterQuery.toLowerCase();
-      return selection.indexOf(lowercaseQuery) > -1;
+      if(selection.indexOf(lowercaseQuery) > -1 && map.thumbnail){
+        return true;
+      }else{
+        return false;
+      }
     }
   },
   created() {
